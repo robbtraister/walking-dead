835d1d0ca670beda06fc226dfda20e6b235dafd1
Robb Traister <robb.traister@gmail.com>
2017-08-02 01:24:07 -0400

    format cover/titlepage

--- package.sh
+++ package.sh
@@ -14,3 +14,16 @@ cd -
 
 mkdir -p `dirname "$2"`
 /Applications/calibre.app/Contents/console.app/Contents/MacOS/ebook-convert "$TEMP_FILE" "$2" --input-profile=default --output-profile=default
+
+rm -rf "$1"
+mkdir -p "$1"
+unzip "$2" -d "$1"
+sed -i "" s/none/xMidYMid/g "$1"/titlepage.xhtml
+sed -i "" s/\<div\>//g "$1"/titlepage.xhtml
+sed -i "" s/\<\\/div\>//g "$1"/titlepage.xhtml
+
+rm -rf "$2"
+TARGET_ZIP=`pwd`/"$2"
+cd "$1"
+zip -r "$TARGET_ZIP" .
+cd -
